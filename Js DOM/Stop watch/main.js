cf16e1611c11b9953a25951c1188e118e49e0a47
oyuerdene <teal.2022.oyuerdene@gamil.com>
2022-12-19 03:37:20 +0800

    .

--- Js DOM/Stop watch/main.js
+++ Js DOM/Stop watch/main.js
@@ -6,26 +6,9 @@ let minuteEl = document.getElementsByClassName('minute')[0]
 let secondEl = document.getElementsByClassName('second')[0]
 let centisecondEl = document.getElementsByClassName('centisecond')[0]
 
-startEl.style.cssText = "background-color: #4caf50; font-size:20px; border: none; border-radius: 50%; width: 100px; height: 100px; color: white; cursor: pointer;"
-stopEl.style.cssText = "background-color: #f44336; font-size:20px; border: none; border-radius: 50%; width: 100px; height: 100px; color: white; cursor: pointer;"
-resetEl.style.cssText = "background-color: #2196f3; font-size:20px; border: none; border-radius: 50%; width: 100px; height: 100px; color: white; cursor: pointer;"
-
-let second = 00
-let minute = 00
-let centisecond = 00
 
 function start(){
-    // let time = 0
-    // setInterval(centisecond, 10)
-    // function centisecond(){
-    //   centisecondEl.innerHTML = time
-    //   time++
-    // }
 
-    // if(centisecondEl.innerHTML == 100){
-    //     centisecondEl
-    // }
-    startEl.style.backgroundColor = 'black'
 }
 
 
